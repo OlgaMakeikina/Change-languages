34fe23cd8410f12f67305457e85a57bca8aa6078
OlgaMakeikina <makeikina2202@gmail.com>
2023-12-20 19:47:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useTranslation } from 'react-i18next';
 
-function App() {
+function App () {
+  const { i18n, t } = useTranslation();
+
+  const toggleLang = () => {
+    i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<h1>{t('Welcome to React')}</h1>
+<button onClick= {toggleLang}>
+{i18n.language === 'en' ? 'RU' : 'EN'}
+</button>
     </div>
   );
 }
